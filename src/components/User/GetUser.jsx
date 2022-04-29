c6769d0f827057d32d3004f9d96282da5102efe1
Eva-Renata <all2evi@yahoo.com>
2022-04-29 09:24:30 +0200

    heroku link

--- src/components/User/GetUser.jsx
+++ src/components/User/GetUser.jsx
@@ -1,30 +1,36 @@
-import { useState } from "react"
-import { UpdateUser } from "./UpdateUser"
+import { useState } from "react";
+import { UpdateUser } from "./UpdateUser";
 
-export const GetUser = ({data, setShowUpdate, setSelectedID}) => {
-    const url = 'http://localhost:4000/api/user'
+export const GetUser = ({ data, setShowUpdate, setSelectedID }) => {
+  const url = "https://boiling-castle-70220.herokuapp.com/api/user";
 
-    let options = {
-        method: "DELETE",
-    }
+  let options = {
+    method: "DELETE",
+  };
 
-    const showUpdateHandler = () => {
-        setShowUpdate(true)
-        setSelectedID(data.id)
-        console.log("Running showUpdateHandler....")
-    }
+  const showUpdateHandler = () => {
+    setShowUpdate(true);
+    setSelectedID(data.id);
+    console.log("Running showUpdateHandler....");
+  };
 
-    return (
-            <div className="listcontainer">
-                <b>{data.firstname} {data.lastname}</b>
-                <p>Email: {data.email}</p>
-                <p>Password: {data.password}</p>
-                <p>Hold: {data.team_id}</p>
-                <button onClick={(showUpdateHandler)}>Edit</button>
-                <button onClick={() => {
-                    fetch(url + "/" + data.id, options)
-                    console.log('deleted ' + data.firstname)
-                }}>Delete</button>
-            </div>
-    )
-}
+  return (
+    <div className="listcontainer">
+      <b>
+        {data.firstname} {data.lastname}
+      </b>
+      <p>Email: {data.email}</p>
+      <p>Password: {data.password}</p>
+      <p>Hold: {data.team_id}</p>
+      <button onClick={showUpdateHandler}>Edit</button>
+      <button
+        onClick={() => {
+          fetch(url + "/" + data.id, options);
+          console.log("deleted " + data.firstname);
+        }}
+      >
+        Delete
+      </button>
+    </div>
+  );
+};
